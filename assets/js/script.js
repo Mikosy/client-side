e4c77a31550a1ba2caeb1529f408c9a123f5e3ae
Mikosy <miibewuba@gmail.com>
2024-12-01 02:44:51 +0100

    updated

--- assets/js/script.js
+++ assets/js/script.js
@@ -276,20 +276,21 @@ function displayTasks(tasks) {
                     <th scope="col">Assigned Users</th>
                     <th scope="col">Status</th>
                     <th scope="col">Due Date</th>
+                    <th scope="col"></th>
                 </tr>
                 </thead>
                 <tbody>
                 <tr>
-                    <th scope="row"></th>
+                    
                     <td>${task.title}</td>
                     <td>${task.description}</td>
-                    <td colspan="2">${task.assignedUsers.map(user => `<br>${user.name} (${user.email})`).join('')}</td>
+                    <td colspan="2">${task.assignedUsers.map(user => `${user.name} (${user.email})`).join('')}</td>
                     <td>${task.status}</td>
                     <td>${new Date(task.dueDate).toLocaleDateString()}</td>
                     <td>
                     <div class="edit-and-delete">
                         <a href="#" class="deleteTask" data-task-id="${task._id}">
-                            <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor"
+                            <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor"
                                 class="bi bi-trash-fill text-danger" viewBox="0 0 16 16">
                                 <path
                                     d="M2.5 1a1 1 0 0 0-1 1v1a1 1 0 0 0 1 1H3v9a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V4h.5a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1H10a1 1 0 0 0-1-1H7a1 1 0 0 0-1 1H2.5zm3 4a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 .5-.5zM8 5a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7A.5.5 0 0 1 8 5zm3 .5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 1 0z" />
@@ -297,6 +298,7 @@ function displayTasks(tasks) {
                         </a>
                     </div>
                     </td>
+                    <td></td>
                 </tr>
                 </tbody>
             </table>
